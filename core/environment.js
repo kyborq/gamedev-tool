ab035bfb5c91f329e1d2bac542ebf4cc90eec7e6
Konstantin <52314985+kyborq@users.noreply.github.com>
2021-02-27 11:48:47 +0300

    Update environment.js

--- core/environment.js
+++ core/environment.js
@@ -1,13 +1,9 @@
-let loopFlag = true;
-
 window.onload = function () {
   // improvised run by once function
   start();
 
   // improvised infinity loop for drawing
-  if (loopFlag) {
-    setInterval(function () {
-      draw();
-    }, 10);
-  }
+  setInterval(function () {
+    draw();
+  }, 10);
 };
